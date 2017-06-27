44f8283caaf7fe5a38beae3cdc4b237d33bbbb65
Felipe Apablaza <ing.apablaza@gmail.com>
2017-06-27 00:42:03 +0000

    Added documentation

--- src/borc-core/borc/CppProjectParser.cpp
+++ src/borc-core/borc/CppProjectParser.cpp
@@ -5,7 +5,7 @@
 namespace borc {
     std::unique_ptr<Project> CppProjectParser::parser(const std::string &file) {
         std::unique_ptr<CppProject> project;
-
+        
         return project;
     }
 }
--- src/borc-core/borc/CppProjectParser.hpp
+++ src/borc-core/borc/CppProjectParser.hpp
@@ -5,10 +5,16 @@
 #include "ProjectParser.hpp"
 
 namespace borc {
+    /**
+     * @brief C/C++ Project Parser
+     */
     class CppProjectParser : public ProjectParser {
     public:
         virtual ~CppProjectParser();
 
+        /**
+         * @brief Parse a XML file and return a CppProject object.
+         */
         virtual std::unique_ptr<Project> parser(const std::string &file) = 0;
     };
 }

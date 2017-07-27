66975b335a8c083fab708c7e0494187991c3046c
Felipe Apablaza <ing.apablaza@gmail.com>
2017-07-26 23:15:10 -0400

    Added first version of the command line utility

--- src/borc/Program.cpp
+++ src/borc/Program.cpp
@@ -1,8 +1,16 @@
 
 #include <iostream>
+#include <boost/program_options.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    namespace po = boost::program_options;
     
+    auto desc = po::options_description("Allowed options");
+
+    desc.add_options()
+        ("help", "")
+        ("compression", po::value<int>(), "set compression level")
+    ;
+
     return 0;
 }

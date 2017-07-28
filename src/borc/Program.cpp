bff78751f3339568288f9e7f53fdf6011347b405
Felipe Apablaza <ing.apablaza@gmail.com>
2017-07-27 22:52:03 -0400

    Added program options

--- src/borc/Program.cpp
+++ src/borc/Program.cpp
@@ -9,8 +9,25 @@ int main(int argc, char **argv) {
 
     desc.add_options()
         ("help", "")
-        ("compression", po::value<int>(), "set compression level")
+        ("generate,g", po::value<int>(), "generate skeleton project")
+        ("build,b", po::value<int>(), "build project")
+        ("configure,c", po::value<int>(), "configure project")
     ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
+
+    if (vm.count("compression")) {
+        std::cout << "Level compression " << vm["compression"].as<int>() << std::endl;
+    } else {
+        std::cout << "There is no compression level" << std::endl;
+    }
 
     return 0;
 }

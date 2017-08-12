b41d7115fac14f669f2275ce9bc127e01d1ff4dd
Felipe Apablaza <ing.apablaza@gmail.com>
2017-08-12 00:13:55 -0400

    Added initial borcfile

--- src/borc/Program.cpp
+++ src/borc/Program.cpp
@@ -1,10 +1,15 @@
 
 #include <iostream>
+#include <stdexcept>
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
+namespace po = boost::program_options;
+
+int setupProject(const fs::path &path);
 
 int main(int argc, char **argv) {
-    namespace po = boost::program_options;
-    
     auto desc = po::options_description("Allowed options");
 
     desc.add_options()
@@ -18,12 +23,50 @@ int main(int argc, char **argv) {
 
     if (vm.count("help")) {
         std::cout << desc << std::endl;
+
         return 0;
     }
 
     if (vm.count("init")) {
-        std::cout << "Initialized empty project structure on the folder current folder." << std::endl;
+        fs::path current = fs::current_path();
+        
+        return setupProject(current);
     }
+
+    std::cout << desc << std::endl;
+
+    return 0;
+}
+
+void checkEmpty(const fs::path &path) {
+    const bool empty = true;
+
+    if (!empty) {
+        throw std::runtime_error("The folder '" + path.string() + "' isn't empty.");
+    }
+}
+
+int setupProject(const fs::path &path) {
+    checkEmpty(path);
+
+    // by default, take the current folder name as the project name
+    const std::string name = path.filename().string();
+
+    // generate the project structure
+    /*
+        project 
+          borcfile
+          src
+    */
+
+    // generate the main YAML description file
+    /*
+        project: borc
+        library: borc-core
+        program: borc
+        program: borc-tdd
+    */
+    std::cout << "Initialized empty '" << name << "' project structure in '" << path.string() << "'" << std::endl;
 
     return 0;
 }

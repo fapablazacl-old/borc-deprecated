c13e56cd4aa443dc99b71b9d9490d1fb65fdcee5
Felipe Apablaza <ing.apablaza@gmail.com>
2017-09-19 23:57:58 -0300

    Added partial implementation for the build task dependencies of targets and sources

--- src/borc/borc/toolchains/Toolchain.cpp
+++ src/borc/borc/toolchains/Toolchain.cpp
@@ -2,12 +2,14 @@
 
 #include "Toolchain.hpp"
 
-#include <cassert>
-
 #include "Compiler.hpp"
 #include "Linker.hpp"
 
+#include <cassert>
 #include <algorithm>
+#include <iostream>
+
+#include <borc/tasks/Task.hpp>
 
 #include <borc/pom/Project.hpp>
 #include <borc/pom/Target.hpp>
@@ -41,6 +43,57 @@ namespace borc {
     void Toolchain::setupTaskHierarchy(TaskHierarchy *hierarchy, const Project *project) {
         assert(hierarchy);
         assert(project);
-        
+
+        auto targets = project->getTargets();
+
+        auto linkers = this->getLinkers();
+        auto compilers = this->getCompilers();
+
+        for (Target *target : targets) {
+            Linker *linker = nullptr;
+
+            for (Linker *linker_ : linkers) {
+                if (linker_->isLinkable(target)) {
+                    linker = linker_;
+                    break;
+                }
+            }
+
+            if (!linker) {
+                std::cout 
+                    << "The target " << target->getName() 
+                    << " could not be built. No supported toolchain found." << std::endl;
+
+                continue;
+            }
+
+            auto sources = target->getSources();
+
+            for (const Source *source : sources) {
+                Compiler *compiler = nullptr;
+
+                for (Compiler *compiler_ : compilers) {
+                    if (compiler_->isCompilable(source)) {
+                        compiler = compiler_;
+                        break;
+                    }
+                }
+
+                if (!compiler) {
+                    std::cout 
+                        << "The source " << source->getFileName()
+                        << " could not be built. No supported compiler build tool found." << std::endl;
+
+                    continue;
+                }
+
+                auto sourceBuildTask = compiler->createTask(source);
+
+                // TODO: Add to the build hierarchy
+                // TODO: Add to the current list of tasks for the build of the target
+                // TODO: Create the dependency relation between the target task and the source tasks 
+                // TODO: Create the task list to make the dependencies between sources
+            }
+        }
     }
 }
